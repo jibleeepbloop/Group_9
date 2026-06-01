aee28fdc28e7577fac0a3b824ae3d2245b8c2ef0
Alex <alexzandracervz@gmail.com>
2026-06-01 22:21:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1801,4 +1801,4 @@ def admin_page():
 
 # ---------- Run ----------
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
